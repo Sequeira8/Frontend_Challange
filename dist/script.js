6106a0e52e5bdc19c9024e46916db5d66c2c11b9
Sequeira8 <carlos.m.r.sequeira@gmail.com>
2021-02-22 10:36:15 +0000

    Developed menu for the other buttons

--- dist/script.js
+++ dist/script.js
@@ -1,2 +1,2 @@
-$(".user_btn").click((function(){$("nav ul .feat-show").toggleClass("show")}));
+$(".user_btn").click((function(){$("nav ul .feat-show").toggleClass("show")})),$(".user_btn2").click((function(){$("nav ul .feat-show2").toggleClass("show")})),$(".user_btn3").click((function(){$("nav ul .feat-show3").toggleClass("show")})),$(".user_btn4").click((function(){$("nav ul .feat-show4").toggleClass("show")})),$(".user_btn5").click((function(){$("nav ul .feat-show5").toggleClass("show")})),$(".user_btn6").click((function(){$("nav ul .feat-show6").toggleClass("show")})),$(".user_btn7").click((function(){$("nav ul .feat-show7").toggleClass("show")})),$(".user_btn8").click((function(){$("nav ul .feat-show8").toggleClass("show")}));
 //# sourceMappingURL=script.js.map
