9d6a45f490170a229c23e3d434fc6a9c132b5891
Ruipqn <ruinobrega19@gmail.com>
2021-02-22 01:24:29 +0000

    click opens/closes sub menu

--- dist/script.js
+++ dist/script.js
@@ -1,2 +1,2 @@
-
+$(".user_btn").click((function(){$("nav ul .feat-show").toggleClass("show")}));
 //# sourceMappingURL=script.js.map
---
+++ js/script.js
@@ -0,0 +1,4 @@
+$('.user_btn').click(function(){
+    $('nav ul .feat-show').toggleClass("show");
+  });
+
